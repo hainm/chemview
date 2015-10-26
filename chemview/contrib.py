3a7991592f217ba5c7c3a65180dae9f979f4f36b
Hai Nguyen <hainm.comp@gmail.com>
2015-10-25 21:26:40 -0400

    add pytraj contrib; not tested yet

--- chemview/contrib.py
+++ chemview/contrib.py
@@ -18,3 +18,23 @@ def topology_mdtraj(traj):
     top['residue_indices'] = [ [a.index for a in r.atoms] for r in traj.topology.residues ]
 
     return top
+
+def topology_pytraj(traj):
+    '''Generate topology spec for the MolecularViewer from pytraj.
+
+    :param pytraj.Trajectory or pytraj.TrajectoryIteratory (out of memory) traj: the trajectory
+    :return: A chemview-compatible dictionary corresponding to the topology defined in pytraj.
+
+    '''
+    import pytraj as pt
+    # TODO: make pytraj's API similiar to mdtraj so we don't need to have two methods?
+
+    top = {}
+    top['atom_types'] = [a.element.symbol for a in traj.topology.atoms]
+    top['atom_names'] = [a.name for a in traj.topology.atoms]
+    top['bonds'] = traj.topology.bond_indices.tolist()
+    top['secondary_structure'] = pt.dssp(traj[:1])[1]
+    top['residue_types'] = [r.name for r in traj.topology.residues ]
+    top['residue_indices'] = [ list(range(r.first_atom_idx, r.last_atom_idx) for r in traj.topology.residues ]
+
+    return top
